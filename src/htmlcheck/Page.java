25d0003f7d698c6eddd3ec252e0d0d070291497b
Carlos Villela <cv@lixo.org>
2009-02-15 16:36:58 +0000

    gotta remove this Page class later on -- just send Elements or Strings around, dunno yet 

--- src/htmlcheck/Page.java
+++ src/htmlcheck/Page.java
@@ -1,18 +1,24 @@
 package htmlcheck;
 
+import java.io.StringReader;
+
 import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
 
 public class Page {
 
+	private String page;
+	
 	public Page(String page) {
+		this.page = page;
 	}
 
 	public String getSource() {
-		return null;
+		return page;
 	}
 
-	public Element getRoot() {
-		return null;
+	public Element getRoot() throws Exception {
+		return new SAXBuilder().build(new StringReader(page)).getRootElement();
 	}
 
 }
