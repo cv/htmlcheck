b857ee62a7ab44c2a5de51549ae04c5c4493a982
Carlos Villela <cv@lixo.org>
2009-02-15 15:25:47 +0000

    added a dummy StaticHtmlDriver... refactoring it out asap

---
+++ src/htmlcheck/StaticHtmlDriver.java
@@ -0,0 +1,24 @@
+package htmlcheck;
+
+import java.util.List;
+
+import org.jdom.Element;
+
+public class StaticHtmlDriver {
+
+	public String getDocument() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Element getRoot() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getElementValues(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
